be19e31f2e7c6938f62fce00709624fb86553987
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-04-01 00:38:11 +0900

    update

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,46 +1,74 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import texture from './textures/earth.jpg'
 
-// add scene
-const scene = new THREE.Scene()
+let scene: THREE.Scene
+let camera: THREE.Camera
+let pointLight: THREE.PointLight
+let renderer: THREE.WebGLRenderer
 
-// add camera
-const camera = new THREE.PerspectiveCamera(
-  50,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-)
-camera.position.set(0, 0, 500)
+const init = () => {
+  // add scene
+  scene = new THREE.Scene()
 
-// add renderer
-const renderer = new THREE.WebGLRenderer({
-  alpha: true,
-})
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+  // add camera
+  camera = new THREE.PerspectiveCamera(
+    50,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
+  camera.position.set(0, 0, 500)
 
-// add geometry
-const ballGeometry = new THREE.SphereGeometry(100, 64, 32)
+  // add renderer
+  renderer = new THREE.WebGLRenderer({
+    alpha: true,
+  })
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setPixelRatio(window.devicePixelRatio)
+  document.body.appendChild(renderer.domElement)
 
-// add material
-const ballMaterial = new THREE.MeshPhysicalMaterial()
+  // add texture
+  const textures = new THREE.TextureLoader().load(texture)
 
-// add mesh
-const ballMesh = new THREE.Mesh(ballGeometry, ballMaterial)
-scene.add(ballMesh)
+  // add geometry
+  const ballGeometry = new THREE.SphereGeometry(100, 64, 32)
 
-// add directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2)
-directionalLight.position.set(1, 1, 1)
-scene.add(directionalLight)
+  // add material
+  const ballMaterial = new THREE.MeshPhysicalMaterial({ map: textures })
 
-// add point light
-const pointLight = new THREE.PointLight(0xffffff, 1)
-pointLight.position.set(-200, -200, -200)
-scene.add(pointLight)
+  // add mesh
+  const ballMesh = new THREE.Mesh(ballGeometry, ballMaterial)
+  scene.add(ballMesh)
 
-// add pointLightHelper
-let pointLightHelper = new THREE.PointLightHelper(pointLight, 30)
-scene.add(pointLightHelper)
+  // add directional light
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 2)
+  directionalLight.position.set(1, 1, 1)
+  scene.add(directionalLight)
 
-renderer.render(scene, camera)
+  // add point light
+  pointLight = new THREE.PointLight(0xffffff, 1)
+  pointLight.position.set(-200, -200, -200)
+  scene.add(pointLight)
+
+  // add pointLightHelper
+  let pointLightHelper = new THREE.PointLightHelper(pointLight, 30)
+  scene.add(pointLightHelper)
+
+  // mouse move
+  let controls = new OrbitControls(camera, renderer.domElement)
+
+  animate()
+}
+
+const animate = () => {
+  pointLight.position.set(
+    200 * Math.sin(Date.now() / 500),
+    200 * Math.sin(Date.now() / 1000),
+    200 * Math.cos(Date.now() / 500)
+  )
+  renderer.render(scene, camera)
+  requestAnimationFrame(animate)
+}
+
+window.addEventListener('load', init)
